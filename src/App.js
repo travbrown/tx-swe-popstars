c4de8542ad24e75b5fab77f1c18263d5cb420866
laurenfraser2 <laurenfraser222@gmail.com>
2020-04-04 19:18:11 -0700

    Fixed spotify login and used routers

--- src/App.js
+++ src/App.js
@@ -1,104 +1,31 @@
-import React, { Component } from 'react';
-import logo from './popstarslogo.png';
+import React from 'react';
 import './App.css';
-import * as $ from "jquery";
-import Player from "./Player";
-import Button from '@material-ui/core/Button';
-export const authEndpoint = 'https://accounts.spotify.com/authorize?';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import GuestLoginPage from './GuestLogin';
+import SpotifyLoginPage from './SpotifyLoginPage';
+import SpotifyLogin from './SpotifyLogin';
+import logo from './popstarslogo.png';
 
-const clientId = "d686743392b64810a7a8c2b5d56bf5c6";
-const redirectUri = "http://localhost:3000/callback/";
-const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-];
-// Get the hash of the url
-const hash1 = window.location.hash
-  .substring(1)
-  .split("&")
-  .reduce(function (initial, item) {
-    if (item) {
-      var parts = item.split("=");
-      initial[parts[0]] = decodeURIComponent(parts[1]);
-    }
-    return initial;
-  }, {});
-window.location.hash = "";
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <body>
+        <Router>
+          <div>
+            <Switch>
+              <SpotifyLoginPage />
+              <Route path="/SpotifyLogin"></Route>
+            </Switch>
+          </div>
+        </Router>
+      </body>
+    </div>
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      token: null,
-      item: {
-        album: {
-          images: [{ url: "" }]
-        },
-        name: "",
-        artists: [{ name: "" }],
-        duration_ms: 0,
-      },
-      is_playing: "Paused",
-      progress_ms: 0
-    };
 
-    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
-  }
 
-  getCurrentlyPlaying(token) {
-    // Make a call using the token
-    $.ajax({
-      url: "https://api.spotify.com/v1/me/player",
-      type: "GET",
-      beforeSend: (xhr) => {
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
-      },
-      success: (data) => {
-        this.setState({
-          item: data.item,
-          is_playing: data.is_playing,
-          progress_ms: data.progress_ms,
-        });
-      }
-    });
-  }
-
-  componentDidMount() {
-    // Set token
-    let _token = hash1.access_token;
-    if (_token) {
-      // Set token
-      this.setState({
-        token: _token
-      });
-    }
-  }
-  render() {
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {!this.state.token && (
-            <a
-              className="btn btn--loginApp-link"
-              href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
-            >
-              Login to Spotify
-        </a>
-          )}
-          {this.state.token && (
-        <Player
-          item={this.state.item}
-          is_playing={this.state.is_playing}
-          progress_ms={this.progress_ms}
-        />
-      )}
-     </header>
-   </div>
   );
- }
 }
-export default App;
-
 
