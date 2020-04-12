da11e5c08256363af90d76752f9cff55dd60bd84
Latifat <latifat.ozoya@bison.howard.edu>
2020-04-11 19:20:01 -0700

    Redirects to difficulty page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import './App.css';
-import GuestLoginPage from './GuestLogin';
-import SpotifyLoginPage from './SpotifyLoginPage';
-import SpotifyLogin from './SpotifyLogin';
-import logo from './popstarslogo.png';
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import "./App.css";
+import SpotifyLoginPage from "./SpotifyLoginPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  Redirect
+} from "react-router-dom";
 import DifficultyPage from "./pages";
 import StartPage from "./pages/start_game";
 import CountdownPage from "./pages/countdown";
@@ -17,36 +20,33 @@ import PlayerTwoGamePage from "./pages/playerTwoGame";
 import TimeUp1 from "./pages/timeUp1";
 import TimeUp2 from "./pages/timeUp2";
 
+function AllLogins() {
+  return (
+    <div>
+      <SpotifyLoginPage />
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br></br>
-        <Router>
-          <div>
-            <Switch>
-              <SpotifyLoginPage />
-              <Route path="/SpotifyLogin"> Login</Route>
-            </Switch>
-          </div>
-        </Router>
-        <Router>
-      <Switch>
-        <Route exact path="/" component ={DifficultyPage} />
-        <Route exact path="/start_game" component ={StartPage} />
-        <Route exact path="/countdown" component ={CountdownPage} />
-        <Route exact path="/gamePage" component ={GamePage} />
-        <Route exact path="/multiplayerPage" component ={MultiplayerPage} />
-        <Route exact path="/playerOne" component ={PlayerOnePage} />
-        <Route exact path ="/playerOneGame" component = {PlayerOneGamePage} />
-        <Route exact path="/playerTwo" component ={PlayerTwoPage} />
-        <Route exact path ="/playerTwoGame" component = {PlayerTwoGamePage} />
-        <Route exact path ="/timeUp1" component = {TimeUp1} />
-        <Route exact path ="/timeUp2" component = {TimeUp2} />
-      </Switch>
-    </Router>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={AllLogins} />
+          <Route exact path="/index" component={DifficultyPage} />
+          <Route exact path="/start_game" component={StartPage} />
+          <Route exact path="/countdown" component={CountdownPage} />
+          <Route exact path="/gamePage" component={GamePage} />
+          <Route exact path="/multiplayerPage" component={MultiplayerPage} />
+          <Route exact path="/playerOne" component={PlayerOnePage} />
+          <Route exact path="/playerOneGame" component={PlayerOneGamePage} />
+          <Route exact path="/playerTwo" component={PlayerTwoPage} />
+          <Route exact path="/playerTwoGame" component={PlayerTwoGamePage} />
+          <Route exact path="/timeUp1" component={TimeUp1} />
+          <Route exact path="/timeUp2" component={TimeUp2} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/GuestLogin.jsx
+++ src/GuestLogin.jsx
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import logo from './popstarslogo.png';
-import './App.css';
-import * as $ from "jquery";
-import Player from "./Player";
-import { Link } from "react-router-dom";
+// import React, { useState, useEffect } from 'react';
+// import logo from './popstarslogo.png';
+// import './App.css';
+// import * as $ from "jquery";
+// import Player from "./Player";
+// import { Link } from "react-router-dom";
 
 
 
 
-  const GuestLogin = () => {
+//   const GuestLogin = () => {
 
-  return (
+//   return (
 
         
-          <Link to="/GuestLogin" class= "active_button">Login as a Guest</Link>
+//           <Link to="/GuestLogin" class= "active_button">Login as a Guest</Link>
 
-  );
-}
+//   );
+// }
 
-export default GuestLogin;
+// export default GuestLogin;
 
 
