fd54f1049fcc634c0914686835dff40658e00ae5
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2020-04-14 12:15:48 -0700

    Imported Firebase into app.js file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Switch,
 } from "react-router-dom";
 import SpotifyLoginPage from "./SpotifyLoginPage";
+import firebase from "firebase";
 import DifficultyPage from "./pages/difficultyPage";
 import StartPage from "./pages/start_game";
 import CountdownPage from "./pages/countdown";
@@ -28,6 +29,47 @@ function AllLogins() {
 }
 
 export default function App() {
+  console.log("App!!!");
+ 
+  /*
+  firebase.firestore().collection("kristak").doc("testID").get().then(function(doc) {
+      if (doc.exists) {
+          console.log("Document data:", doc.data());
+      } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+      }
+  }).catch(function(error) {
+      console.log("Error getting document:", error);
+  });
+  */
+   //Added a new collection, users
+ firebase.firestore().collection("Users").doc("Tariq").set({
+  Username : "Tariqqqq123",
+  Score : 325
+ })
+ .then(function() {
+  console.log("Document successfully written!");
+ })
+ .catch(function(error) {
+  console.error("Error writing document: ", error);
+ });
+  
+ //Leaderboard Code
+ var leader = firebase.firestore().collection("Users");
+ var query = leader.where("Score",">=", 325);
+ query.get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+ 
+
   return (
     <div className="App-login ">
       <Router>
