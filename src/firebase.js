1681dbd0cda80450604c8b3692dda2c731ea53ae
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2020-04-14 10:38:55 -0700

    Changed the firebase config to the official
    Changed the Firebase configuration in this file to the one that I have been doing the leaderboard on.

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'; 
  
 const firebaseConfig = {
-    apiKey: "AIzaSyCvKX8bYRMxuN6hNtcmlakAqPF_vVMbv7k",
-    authDomain: "popstars.firebaseapp.com",
-    databaseURL: "https://popstars.firebaseio.com",
-    projectId: "popstars",
-    storageBucket: "popstars.appspot.com",
-    messagingSenderId: "228302208581",
-    appId: "1:228302208581:web:fc91c851f88a230319ed2c",
-    measurementId: "G-QV0XR06Y4L"
+  apiKey: "AIzaSyBC-mDXhxigV5NiXRkwOVhZOyxjvuAwcWA",
+  authDomain: "popstars-76cdd.firebaseapp.com",
+  databaseURL: "https://popstars-76cdd.firebaseio.com",
+  projectId: "popstars-76cdd",
+  storageBucket: "popstars-76cdd.appspot.com",
+  messagingSenderId: "826536627753",
+  appId: "1:826536627753:web:a4b9eace9f134d4f019638",
+  measurementId: "G-7ES8P675LN"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
