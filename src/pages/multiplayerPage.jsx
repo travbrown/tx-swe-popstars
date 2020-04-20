04dc18afe0d60d8c0726e49f0b96b8182fb347b3
laurenfraser2 <laurenfraser222@gmail.com>
2020-04-19 17:06:54 -0700

    Name and subject are on same line if you go to multiplayer page- pre-update for all pages

--- src/pages/multiplayerPage.jsx
+++ src/pages/multiplayerPage.jsx
@@ -5,13 +5,13 @@ import {PlayerOneName} from './multiplayerNames';
 var name1 = localStorage.getItem('name1'); 
 const MultiplayerPage = () => {
     return (
+
         <div className="App">
-          <div class ="item">
-            <h2>{name1}</h2>
-            <li id="username">Username </li>
-            <li id="subject"><center>Difficulty </center></li>
-          </div>
-          
+          <nav class="item">
+            <h2 id="username"> {name1} </h2>
+            <h2 id="subject"> Difficulty </h2>
+          </nav>
+       
           <header >
             <div className="centerItems">
             <img src={logo} className="App-logo" alt="logo" height = "300px" />
