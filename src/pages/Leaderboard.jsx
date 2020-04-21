d033288555362c694ce5bec437e785526fd575f7
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2020-04-21 12:05:06 -0700

    Leaderboard is now fully functional on the backend

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,6 +1,5 @@
 import firebase from "firebase";
-import gamepage from "./gamePage.jsx";
-import React from "react";
+
 
 console.log("App!!!");
 var leader = firebase.firestore().collection("Users").where("Score",">", 0).orderBy("Score","desc").limit(10);
@@ -8,21 +7,24 @@ var leader = firebase.firestore().collection("Users").where("Score",">", 0).orde
 //console.log(Leaderboard);
 var userScore = parseInt(localStorage.getItem('User Score'));
 
-//var name1 = localStorage.getItem("playerOneName");
+var name = localStorage.getItem("name1");
 
 //const multiplayer = localStorage.getItem("mulitplayer");
 
 // "'" + name1 + "'"  // This will input the variable as a name 
    
-        firebase.firestore().collection("Users").doc("Krazy Kam").set({
+        firebase.firestore().collection("Users").doc("'" + name + "'").set({
             Score : userScore
            })
            .then(function() {
             console.log("Document successfully written!");
+            localStorage.clear();
            })
            .catch(function(error) {
             console.error("Error writing document: ", error);
            });
+
+        
 
         //Leaderboard Code
     //const Leaderboard = () => { 
