36d139ea12f3700ee00a72dc9f5ae81e1108e7a2
Latifat <latifat.ozoya@bison.howard.edu>
2020-04-21 23:34:29 -0700

    multiplayer page works well

--- src/pages/playerTwoGame.jsx
+++ src/pages/playerTwoGame.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Howl } from "howler";
 import SpotifyWebApi from "spotify-web-api-js";
+import DisplayScore from "./displayScore.jsx";
+import PlayerTwoBubble from "./playerTwoContainer.jsx";
 import "./gamePage.css";
 import { Link } from "react-router-dom";
 import asap_ferg from "../photos/ASAP_Ferg.png";
@@ -13,161 +15,152 @@ import kanye_west from "../photos/kanye_west.png";
 import jcole from "../photos/jcole.png";
 import nicki_minaj from "../photos/nicki_minaj.png";
 import beyonce from "../photos/Beyonce.png";
+import davido from "../photos/davido.jpg";
+import Justin_beiber from "../photos/Justin_beiber.jpg";
+import lizzo from "../photos/lizzo.jpeg";
+import rihanna from "../photos/rihanna.jpg";
+import wiz_khalifa from "../photos/wiz_khalifa.png";
 
 let artistsFaces = [
-  { name: 'A$AP Ferg', image: asap_ferg },
-  { name: 'A$AP Rocky', image: asap_rocky },
-  { name: 'Cardi B', image: cardi_b },
-  { name: 'Drake', image: drake },
-  { name: 'Lil Wayne', image: lil_wayne },
-  { name: '2Pac', image: tupac },
-  { name: 'Kanye West', image: kanye_west },
-  { name: 'J. Cole', image: jcole },
-  { name: 'Nicki Minaj', image: nicki_minaj },
-  { name: 'Beyoncé', image: beyonce },
+    { name: "A$AP Ferg", image: asap_ferg },
+    { name: "A$AP Rocky", image: asap_rocky },
+    { name: "Cardi B", image: cardi_b },
+    { name: "Drake", image: drake },
+    { name: "Lil Wayne", image: lil_wayne },
+    { name: "2Pac", image: tupac },
+    { name: "Kanye West", image: kanye_west },
+    { name: "J. Cole", image: jcole },
+    { name: "Nicki Minaj", image: nicki_minaj },
+    { name: "Beyoncé", image: beyonce },
+    { name: "Davido", image: davido },
+    { name: "Justin Beiber", image: Justin_beiber },
+    { name: "Lizzo", image: lizzo },
+    { name: "Rihanna", image: rihanna },
+    { name: "Wiz Khalifa", image: wiz_khalifa },
 ];
-  
+
 const PlayerTwoGame = () => {
-	const spotifyApi = new SpotifyWebApi();
+    const spotifyApi = new SpotifyWebApi();
 
-	const [playlist, setPlaylist] = useState(null);
-	const [artists, setArtists] = useState(null);
-	const [track, setTrack] = useState(null);
+    const [playlist, setPlaylist] = useState(null);
+    const [artists, setArtists] = useState(null);
+    const [track, setTrack] = useState(null);
 
-	const [songIndex, setSongIndex] = useState(0);
-	const [artistIndex, setArtistIndex] = useState(0);
+    const [songIndex, setSongIndex] = useState(0);
+    const [artistIndex, setArtistIndex] = useState(0);
 
-	const [soundHowl, setSoundHowl] = useState(null);
-	const [score, setScore] = useState(0);
+    const [soundHowl, setSoundHowl] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+  
+    const ref = useRef(null);
+
+    const wrapperSetScore = delta => {
+        ref.current.addToScore(delta);
+     };
+
+    const shuffle = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+        }
+    };
 
 
-	const Bubble = ({ number, hasArtist, image, name }) => {
-    
-		const [clicked, setClicked] = useState(false);
-		const checkAnswer = () => {
-			setClicked(true);
-			if (name === artists[artistIndex]) {
-				setScore(score + 5);
-				nextSong();
-			}
-			// else if (artistIndex === artists.length-1) {
-            //     window.location.href="/timeUp2";
-            // }
-            else {
-                setScore(score - 2);
+  const nextSong = () => {
+    shuffle(artistsFaces);
+    soundHowl.stop();
+    setArtistIndex(artistIndex + 1);
+    playMusic();
+  };
+
+  useEffect(() => {
+    let token = localStorage.getItem("access_token");
+    makeSpotifyCall(token);
+  }, []);
+
+  const makeSpotifyCall = async token => {
+    await getPlaylist(token);
+  };
+
+  const getPlaylist = async access_token => {
+    spotifyApi.setAccessToken(access_token);
+    //Couldn't get the Promise implementation to work
+    //spotifyApi.setPromiseImplementation(Q);
+    await spotifyApi
+      .getPlaylistTracks("4h4V4Cbn8sjznAc3uirZmK")
+      .then(
+        function(data) {
+          let foundSongs = [];
+          let artist = [];
+          data.items.forEach(item => {
+            if (item.track.preview_url !== null && foundSongs.length < 10) {
+              artist.push(item.track.artists[0].name);
+              foundSongs.push(item.track.preview_url);
             }
-    	};
-	
-		return (
-			<div class={`bubble x${number}`}  
-			style={{ display: clicked ? "none" : "flex" }}
-			onClick={checkAnswer} >
-			<img
-				style={{
-				display: hasArtist ? "inline-block" : "none",
-				height: "100%",
-				width: "100%",
-				objectFit: "cover",
-				borderRadius: "50%"
-				}}
-				alt= "bubble pic"
-				src={image}
-			/>
-			</div>
-		);
-	};
+          });
+          setTrack(foundSongs[songIndex]);
+          setArtists(artist);
+          setPlaylist(foundSongs);
+        },
+        function(err) {
+          console.error(err);
+        }
+      )
+      .catch(e => console.log(e));
+    document.getElementById("autoPlay").click();
+  };
 
-    const nextSong = () => {
-		soundHowl.stop();
-		setArtistIndex(artistIndex+1);
-		playMusic();
-	}
+  useEffect(() => {
+    if (soundHowl) soundHowl.play();
+  }, [soundHowl]);
+
+  const playMusic = () => {
+    setSoundHowl(
+      new Howl({
+        src: [track],
+        html5: true,
+        format: ["mp3", "aac"],
+        autoplay: false,
+        loop: false,
+        volume: 0.5,
+        onload: function() {
+          setTrack(playlist[songIndex + 1]);
+          setSongIndex(songIndex + 1);
+        },
+        onend: function() {
+        }
+      })
+    );
+  };
+
+  shuffle(artistsFaces);
  
-	const makeSpotifyCall = async (token) => {
-		await getPlaylist(token);
-	};
-
-	useEffect(() => {
-		let token = localStorage.getItem('access_token');
-		makeSpotifyCall(token);
-	}, []);
-
-	const getPlaylist = async access_token => {
-		spotifyApi.setAccessToken(access_token);
-		//Couldn't get the Promise implementation to work
-		//spotifyApi.setPromiseImplementation(Q);
-		await spotifyApi
-		.getPlaylistTracks("4h4V4Cbn8sjznAc3uirZmK")
-		.then(
-			function(data) {
-			let foundSongs = [];
-			let artist = [];
-			data.items.forEach(item => {
-				if (item.track.preview_url !== null && foundSongs.length < 10) {
-				artist.push(item.track.artists[0].name);
-				foundSongs.push(item.track.preview_url);
-				}
-			});
-			setTrack(foundSongs[songIndex]);
-			setArtists(artist);
-			setPlaylist(foundSongs);
-			},
-			function(err) {
-			console.error(err);
-			}
-		)
-		.catch(e => console.log(e));
-		document.getElementById("autoPlay").click();
-	};
-
-	useEffect(() => {
-		if (soundHowl) soundHowl.play();
-	}, [soundHowl]);
-	
-	const playMusic = () => {
-		setSoundHowl(
-				new Howl({
-				src: [track],
-				html5: true,
-				format: ["mp3", "aac"],
-				autoplay: false,
-				loop: false,
-				volume: 0.5,
-				onload: function() {
-					console.log("LOADED!!");
-					setTrack(playlist[songIndex+1]);
-					setSongIndex(songIndex+1);
-				},
-				onend: function() {
-					console.log("Finished!");
-				},
-			})
-		);
-	};
-
   return (
     <div className="App">
-		<button
-			id="autoPlay"
-			style={{ display: "none" }}
-			onClick={playMusic}> 
-			can you see me? 
-		</button>
-		<div class="item">Username</div>
-		<div class="item">SCORE: {score}</div>
-		
-		<div id="background-wrap">
-		{artistsFaces.map((item, idx) => (
-			<>
-			<Bubble
-				key={idx}
-				image={item.image}
-				hasArtist
-				number={idx}
-				name={item.name} />
-			</>
-		))}
-		</div>
+      <button id="autoPlay" style={{ display: "none" }} onClick={playMusic}>
+        can you see me?
+      </button>
+      
+      <div class="item">Username</div>
+      <div class="item">SCORE: <DisplayScore ref={ref} /></div>
+      
+      <div id="background-wrap">
+        {artistsFaces.map((item, idx) => (
+          <>
+            <PlayerTwoBubble
+              key={item.name}
+              image={item.image}
+              hasArtist
+              number={idx}
+              name={item.name}
+              wrapperSetScore = {wrapperSetScore}
+              artists = {artists}
+              artistIndex = {artistIndex}
+              nextSong = {nextSong}
+            />
+          </>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/timeUp1.jsx
+++ src/pages/timeUp1.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import './difficultyPage.css';
 import { Link } from "react-router-dom";
 
+var score = localStorage.getItem('score'); 
+
 const TimeUp1 = () => {
     return (
       <div className="App">
            <header className="App-header">
                 <h1> TIME'S UP!</h1>
-                <h2> Score: 0 </h2>
+                <h2> Score: {score} </h2>
                 <h1> PLAYER TWO'S TURN</h1>
                 <p></p> <p></p>
                 <Link to="/playerTwo" class= "active_button"> START</Link> 
