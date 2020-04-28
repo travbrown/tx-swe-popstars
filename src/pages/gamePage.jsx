154361c7b1b598d6a61a12b3a5e0d8bacd89b552
Latifat <latifat.ozoya@bison.howard.edu>
2020-04-28 10:07:28 -0700

    still WIP

--- src/pages/gamePage.jsx
+++ src/pages/gamePage.jsx
@@ -4,6 +4,9 @@ import SpotifyWebApi from "spotify-web-api-js";
 import DisplayScore from "./displayScore.jsx";
 import Bubble from "./bubbleContainer.jsx";
 import "./gamePage.css";
+import { useHistory } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
 import {GameContext} from './../gameContext';
 import asap_ferg from "../photos/ASAP_Ferg.png";
 import asap_rocky from "../photos/ASAP_Rocky.png";
@@ -20,7 +23,16 @@ import Justin_beiber from "../photos/Justin_beiber.jpg";
 import lizzo from "../photos/lizzo.jpeg";
 import rihanna from "../photos/rihanna.jpg";
 import wiz_khalifa from "../photos/wiz_khalifa.png";
-import { Link, useHistory } from "react-router-dom";
+import megan_thee_stallion from "../photos/megan_thee_stallion.jpg";
+import michael_jackson from "../photos/michael_jackson.jpg";
+import skepta from "../photos/skepta.jpg";
+import post_malone from "../photos/post_malone.png";
+import XXXTentacion from "../photos/XXXTentacion.png";
+import burnaboy from "../photos/burnaboy.jpg";
+import chris_brown from "../photos/chris_brown.jpg";
+import vybz_kartel from "../photos/vybz_kartel.jpg";
+import selena_gomez from "../photos/selena_gomez.png";
+import eminem from "../photos/eminem.png";
 
 let artistsFaces = [
     { name: "A$AP Ferg", image: asap_ferg },
@@ -35,9 +47,19 @@ let artistsFaces = [
     { name: "Beyoncé", image: beyonce },
     { name: "DaVido", image: davido },
     { name: "Justin Bieber", image: Justin_beiber },
-    { name: "Lizzo", image: lizzo },
-    { name: "Rihanna", image: rihanna },
-    { name: "Wiz Khalifa", image: wiz_khalifa },
+    // { name: "Lizzo", image: lizzo },
+    // { name: "Rihanna", image: rihanna },
+    // { name: "Wiz Khalifa", image: wiz_khalifa },
+    // { name: "Megan Thee Stallion", image: megan_thee_stallion },
+    // { name: "Michael Jackson", image: michael_jackson },
+    // { name: "Skepta", image: skepta },
+    // { name: "Post Malone", image: post_malone },
+    // { name: "XXXTENTACION", image: XXXTentacion },
+    // { name: "Burna Boy", image: burnaboy },
+    // { name: "Chris Brown", image: chris_brown },
+    // { name: "Vybz Kartel", image: vybz_kartel },
+    // { name: "Selena Gomez", image: selena_gomez },
+    // { name: "Eminem", image: eminem },
 ];
 
 const GamePage = () => {
@@ -46,16 +68,33 @@ const GamePage = () => {
   const history = useHistory();
 
 	const [playlist, setPlaylist] = useState(null);
-
+  const [bubbleLimit, setBubbleLimit] = useState(getBubbleLimit());
   const [limitOfSongsToPlay, setlimitOfSongsToPlay] = useState(setSongLimit());
 	const [songIndex, setSongIndex] = useState(0);
 	const [showModal, setShowModal] = useState(false);
-
 
   const ref = useRef(null);
   const wrapperSetScore = delta => {
       ref.current.addToScore(delta);
    };
+
+   function getBubbleLimit(){
+    if(difficulty === 'medium'){
+      return 20;
+    } else if (difficulty === 'hard'){
+      return 25;
+    }
+    return 15;
+  };
+
+  function setSongLimit(){
+    if(difficulty === 'medium'){
+      return 10;
+    } else if (difficulty === 'hard'){
+      return 15;
+    }
+    return 7;
+  };
 
   const shuffle = array => {
       for (let i = array.length - 1; i > 0; i--) {
@@ -64,6 +103,27 @@ const GamePage = () => {
       }
   };
 
+  // function RenderPlaylist() {
+  //   if (playlist === null) {
+  //     //load
+  //   }
+  // }
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  const ensureCorrectArtistGetsBubbled = () => {
+    console.log(artistsFaces)
+    console.log(playlist)
+    for (let i = bubbleLimit; i < artistsFaces.length; i++) {
+      if(artistsFaces[i].name === playlist[songIndex].artist_name){
+        let num = getRandomInt(bubbleLimit);
+        [artistsFaces[i], artistsFaces[num]] = [artistsFaces[num], artistsFaces[i]];
+        break;
+      }
+    }
+  }
 
 	const nextSong = () => {
     shuffle(artistsFaces);
@@ -94,7 +154,6 @@ const GamePage = () => {
       console.log(playlist)
       let foundSongs = [];
       for (const item of playlist.items) {
-        
         if (item.track.preview_url == null) continue;
         foundSongs.push({
           artist_name:item.track.artists[0].name, 
@@ -103,7 +162,9 @@ const GamePage = () => {
       }
       shuffle(foundSongs)
       setPlaylist(foundSongs);
-      
+      //RenderPlaylist();
+      console.log(playlist)
+      ensureCorrectArtistGetsBubbled();
     } catch (error) {
       // alert('Our access to Spotify has expired.\nPress OK to login and refresh our access');
       // history.push('/');
@@ -121,18 +182,18 @@ const GamePage = () => {
       />
     );
   
-	let name1 = localStorage.getItem('name1'); 
+  let name1 = localStorage.getItem('name1');  
   shuffle(artistsFaces);
- 
+
   return (
     <div className="App">   
       <nav class="item">
-        <h2 id="username"> {name1}</h2>
+        <h2 id="username"> {name1} </h2>
         <h2 id="subject"> SCORE: <DisplayScore ref={ref} /> </h2>
         <h2 id="end-btn"> <button onClick={() => setShowModal(true)} id="end">QUIT</button></h2>
       </nav>
       <div id="background-wrap">
-        {artistsFaces.map((item, idx) => (
+        {playlist && artistsFaces.slice(0,bubbleLimit).map((item, idx) => (
           <>
             <Bubble
               key={item.name}
@@ -144,7 +205,8 @@ const GamePage = () => {
               playlist = {playlist}
               songIndex = {songIndex}
               nextSong = {nextSong}
-            />
+              difficulty={difficulty}
+            />;
           </>
         ))}
       </div>
@@ -164,5 +226,4 @@ const GamePage = () => {
     </div>
   );
 };
-
 export default GamePage;
