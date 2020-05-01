cb6f3c95dd40311916820e3f81b8ef58694dbbe6
Latifat <latifat.ozoya@bison.howard.edu>
2020-04-30 20:15:23 -0700

    deleted the two extra game over pages

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ import PlayerOneGame from "./pages/playerOneGame";
 import PlayerTwoPage from "./pages/playerTwo";
 import PlayerTwoGame from "./pages/playerTwoGame";
 import TimeUp1 from "./pages/timeUp1";
-import TimeUp2 from "./pages/timeUp2";
 import GameMode from './pages/gameMode';
 import NameOneInput from "./pages/multiplayerNames";
 import GameOver from "./pages/gameOver";
@@ -26,7 +25,6 @@ import SinglePlayerInput from "./pages/singleplayerName";
 import CreateGame from "./pages/createGame";
 import CutOffMark from "./pages/cutOffMark";
 import ChoosePlaylist from "./pages/choosePlaylist";
-import ChallengeOver from "./pages/challengeOver";
 import { GameContext } from "./gameContext";
 
 export default function App() {
@@ -43,7 +41,6 @@ export default function App() {
             <Route exact path="/createGame" component ={CreateGame} />
             <Route exact path="/cutOffMark" component={CutOffMark} />
             <Route exact path="/choosePlaylist" component={ChoosePlaylist} />
-            <Route exact path="/challengeOver" component={ChallengeOver}/>
             <Route exact path="/difficultyPage" component={DifficultyPage} />
             <Route exact path="/countdown" component={CountdownPage} />
             <Route exact path="/gamePage" component={GamePage} />
@@ -54,7 +51,6 @@ export default function App() {
             <Route exact path="/playerTwo" component={PlayerTwoPage} />
             <Route exact path="/playerTwoGame" component={PlayerTwoGame} />
             <Route exact path="/timeUp1" component={TimeUp1} />
-            <Route exact path="/timeUp2" component={TimeUp2} />
             <Route exact path="/multiplayerNames" component={NameOneInput} />
             <Route exact path="/gameOver" component ={GameOver} />
             <Route exact path="/singleplayerName" component={SinglePlayerInput} />
